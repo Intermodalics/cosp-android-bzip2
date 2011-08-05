5c4ac2e19ef988b88e4997cc6f18d74aa6a0bedc
Dmitry Moskalchuk <dm@crystax.net>
2011-08-05 19:33:40 +0400

    Use build script from subtree instead of separate repository

--- build.sh
+++ build.sh
@@ -2,18 +2,7 @@
 
 cd `dirname $0` || exit 1
 
-[ "x$BUILD_REPO" = "x" ] && BUILD_REPO=git://android.git.crystax.net/cosp/android/build.git
-[ "x$BUILD_DIR" = "x" ] && BUILD_DIR=./build
-cwd=`pwd`
-if [ ! -d $BUILD_DIR ]; then
-  cd `dirname $BUILD_DIR` || exit 1
-  git clone $BUILD_REPO `basename $BUILD_DIR` || exit 1
-else
-  cd $BUILD_DIR || exit 1
-  git pull || exit 1
-fi
-cd $cwd || exit 1
-. $BUILD_DIR/build-common.sh || exit 1
+. build/build-common.sh || exit 1
 
 export PREFIX ARCH
 
